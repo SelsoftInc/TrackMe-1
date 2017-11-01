801dbab8db56821392906e3972abada7e6b1d8d5
Sudhanshu Barik <sudhansubarik1983@gmail.com>
2017-11-02 02:58:20 +0530

    commits MongoConfiguraton and implementation of getRentalDetail API

--- TrackMe/src/main/java/com/selsoft/trackme/config/MongoConfiguration.java
+++ TrackMe/src/main/java/com/selsoft/trackme/config/MongoConfiguration.java
@@ -1,10 +1,18 @@
 package com.selsoft.trackme.config;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
@@ -13,17 +21,26 @@ import com.mongodb.MongoClient;
  * @author Sudhansu Sekhar
  *
  */
-@Configuration
-@ComponentScan(basePackages = "com.selsoft.trackme.model")
-public class MongoConfiguration {
 
+@Configuration
+@ComponentScan(basePackages = { "com.selsoft.trackme.*" })
+@PropertySource("classpath:Mongoconfig.properties")
+public class MongoConfiguration {
+	@Autowired
+	private Environment env;
+
+	
+	String mongodbUrl = env.getProperty("mongodb.url");
+	String defaultDb = env.getProperty("mongodb.db");
+
+	
 	@Bean
 	public Mongo mongo() throws Exception {
-		return new MongoClient("MongoConfig.MONGO_HOST");
+		return new MongoClient("mongodbUrl");
 	}
 
 	@Bean
 	public MongoTemplate mongoTemplate() throws Exception {
-		return new MongoTemplate(mongo(), "MongoConfig.MONGO_DB");
+		return new MongoTemplate(mongo(), "defaultDb");
 	}
 }
--- TrackMe/src/main/java/com/selsoft/trackme/dao/LeaseDAOImpl.java
+++ TrackMe/src/main/java/com/selsoft/trackme/dao/LeaseDAOImpl.java
@@ -9,12 +9,14 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.util.JSON;
 import com.selsoft.trackme.constants.TrackMeConstants;
 import com.selsoft.trackme.model.Lease;
 import com.selsoft.trackme.model.Property;
@@ -81,64 +83,40 @@ public class LeaseDAOImpl implements LeaseDAO {
 	@Override
 	public RentalDetail getRentalDetail(Integer propertyId, Date inputDate) {
 
-		RentalDetail rentalDetail = null;
+		Query query = new Query(Criteria.where("propertyId").is(propertyId));
+		query.limit(10);
+		query.with(new Sort(Sort.Direction.DESC, "effectiveDate"));
 
-		if (propertyId != null) {
+		List<RentalDetail> rentalDetailList = template.find(query, RentalDetail.class);
+		logger.info("RECENT RETAL DETAIL: "+rentalDetailList.get(0));
+		
+//		try {
+//		Timestamp currentTimestamp = new Timestamp(inputDate.getTime());
+//
+//			for (RentalDetail rentalDetail : rentalDetailList) {
+//				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+//				Date firstParsedDate = dateFormat.parse(currentTimestamp.toString());
+//				Date secondParsedDate = dateFormat.parse((rentalDetail.getEffectiveDate()).toString());
+//				long diff = secondParsedDate.getTime() - firstParsedDate.getTime();
+//				int days = (int) (diff / (1000 * 60 * 60 * 24));
+//
+//			}
+//		} catch (ParseException p) {
+//
+//		}
+		return rentalDetailList.get(0);
+	}
 
-			Query query = new Query(Criteria.where("propertyId").is(propertyId));
-			Property property = template.findOne(query, Property.class);
-			Date date = property.getRentalDetail().getEffectiveDate();
+	private Date getDateNearest(List<Date> dates, Date targetDate) {
+		Date returnDate = targetDate;
 
-			Timestamp timestamp1 = new Timestamp(date.getTime());
-			Timestamp timestamp2 = new Timestamp(inputDate.getTime());
-
-			//ObjectId.getTimestamp();
-			
-		/*	
-			try
-		    {
-		        SimpleDateFormat sdf = new SimpleDateFormat(timestamp2);
-		        if (sdf.format(sdf.parse(timestamp1)).equals(timestamp1))
-		            return rentalDetail;
-		    }
-		    catch (ParseException pe) {}
-
-		    return null;
-		}
-			
-			db.example.insert({"inputDate":new Date()});
-			Date now = new Date();
-
-			BasicDBObject timeNow = new BasicDBObject("date", now);
-			example.insert(timeNow);
-			
-			BasicDBObject query = new BasicDBObect();
-			 BasicDBObject update = new BasicDBObject("$currentDate",
-			     new BasicDBObject("date", true));
-			 
-			 query.put("date", new Date());
-			 String serialize = JSON.serialize(query);
-			     db.example.insert({"date":ISODate("2016-03-03T08:00:00.000")});
-			 );
-
-			 example.update(query,update,true,false);
-			 
-			 DBObject object = new BasicDBObject();
-			 object.put("date", new Date());
-			 String serialize = JSON.serialize(object)*/;
-			
-				if (timestamp2.compareTo(timestamp1) < 0) {
-				logger.info(" input date is most recently used date");
-
-			                      }
-
-		} else {
-
-			template.save(rentalDetail);
-
+		for (Date date : dates) {
+			if (date.compareTo(targetDate) <= 0 && date.compareTo(returnDate) > 0) {
+				returnDate = date;
+			}
 		}
 
-		return null;
+		return returnDate;
 	}
 
 }
