80618e93e793e15e6c344c8a4bb09a1185db35d2
Sudhanshu Barik <sudhansubarik1983@gmail.com>
2017-11-24 01:20:05 +0530

    JWT Security Demo Application

---
+++ JWTSecurity/src/main/java/com/selsoft/jwt/Application.java
@@ -0,0 +1,19 @@
+package com.selsoft.jwt;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starting point of the application
+ */
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        run(Application.class, args);
+    }
+
+
+}
