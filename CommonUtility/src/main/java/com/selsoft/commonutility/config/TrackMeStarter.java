81b5c0d8ab32e18b30ca1fb0b19cf9d4b8201ee1
selsoft <selsoft@DESKTOP-KSQLQI1>
2017-11-29 04:06:58 -0500

    commits application.properties file,TrackMeStarter of CommonUtility microservice

--- CommonUtility/src/main/java/com/selsoft/commonutility/config/TrackMeStarter.java
+++ CommonUtility/src/main/java/com/selsoft/commonutility/config/TrackMeStarter.java
@@ -1,34 +1,23 @@
 package com.selsoft.commonutility.config;
 
-import javax.servlet.Filter;
-
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 
  * @author Sudhansu Sekhar
  *
  */
-public class TrackMeStarter extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { WebConfig.class };
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages="com.selsoft")
+public class TrackMeStarter extends SpringApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(TrackMeStarter.class);
 	}
 
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return null;
-	}
-
-	@Override
-	protected String[] getServletMappings() {
-		return new String[] { "/" };
-	}
-
-	@Override
-	protected Filter[] getServletFilters() {
-		Filter[] singleton = { new CORSFilter() };
-		return singleton;
-	}
 }
