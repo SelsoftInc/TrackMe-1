244a03c3a202077e75250b38bdf455160e14cf40
selsoft <selsoft@DESKTOP-KSQLQI1>
2017-12-07 05:51:48 -0500

    commits UserContants and UserDto class of TrackMe application.

---
+++ TrackMe/src/main/java/com/selsoft/trackme/constants/UserConstants.java
@@ -0,0 +1,12 @@
+package com.selsoft.trackme.constants;
+
+public class UserConstants {
+
+	public static final String OWNER = "OWN";
+	public static final String MANAGER = "MGR";
+	
+	public static final String TENANT = "TNT";
+	public static final String SUCCESS = "success";
+	
+	
+}
--- TrackMe/src/main/java/com/selsoft/trackme/controller/TransactionController.java
+++ TrackMe/src/main/java/com/selsoft/trackme/controller/TransactionController.java
@@ -1,26 +1,19 @@
 package com.selsoft.trackme.controller;
 
-import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.selsoft.trackme.model.Transaction;
 import com.selsoft.trackme.model.ValidError;
 import com.selsoft.trackme.service.TransactionService;
@@ -30,26 +23,32 @@ import com.selsoft.trackme.service.TransactionService;
 public class TransactionController {
 
 	private static final Logger logger = Logger.getLogger(TransactionController.class);
-	// private static TransactionType transactionType = new TransactionType();
-
 	@Autowired
 	private TransactionService transactionService;
 
 	@RequestMapping(value = "/saveTransaction", method = RequestMethod.PUT)
-	public void saveTransaction(@RequestBody Transaction transaction, @RequestParam("file") MultipartFile file) {
-		/*BufferedInputStream inputStream = null;
+	public void saveTransaction(@RequestParam("transaction") MultipartFile json, @RequestParam("file") MultipartFile file) {
+		Transaction transaction=null;
+		String content="";
+		
 		try {
-			inputStream = new BufferedInputStream(file.getInputStream());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+			 content = new String(json.getBytes());
+		} catch (IOException e3) {
+			e3.printStackTrace();
+		}
+	    
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			transaction = mapper.readValue(content, Transaction.class);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
 		logger.info(transaction.getTransactionId()
 				+ " data comes into TransactionController saveTransaction() for processing");
 		try {
 			transaction.setFile(file.getBytes());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		ValidError validError = transactionService.validateTransactionData(transaction);
@@ -59,15 +58,13 @@ public class TransactionController {
 
 	}
 
-	
 	@RequestMapping(value = "/ getTransaction", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Transaction> getTransaction(long transactionId) {
 		return transactionService.getTransaction(transactionId);
 	}
 
-	
 	@RequestMapping(value = "/ getTransactionsForProperty", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Transaction> getTransactionsForProperty(int  propertyId) {
+	public List<Transaction> getTransactionsForProperty(int propertyId) {
 		return transactionService.getTransactionsForProperty(propertyId);
 	}
 
